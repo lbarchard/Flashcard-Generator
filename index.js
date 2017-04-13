3d3f8f3791d562e45ab8bae193b0edc6570eca8a
Les Barchard <lbarchard@Less-MacBook-Pro.local>
2017-04-12 22:05:29 -0400

    creating post

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
+var express    = require('express');  
+var app        = express();     
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+var clozeCards = [];
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+
+router.use(function(req, res, next) {
+    // do logging
+    console.log(req.originalUrl);
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/clozecard', function(req, res) {
+    res.json({
+    message: 'hooray! welcome to our apis!'
+    // fullText: firstCard.fullText
+});
+    
+
+    // res.json({fullText: firstCard.fullText});
+
+});
+
+// more routes for our API will happen here
+router.route('/clozecards')
+
+    // create a clozecard (accessed at POST http://localhost:8080/api/clozecards)
+    .post(function(req, res) {
+
+        var clozeCard = {};
+        ClozeCard.call(clozeCard, req.body.fullText, req.body.cloze);
+        clozeCards.push(clozeCard);       
+        console.log(clozeCards)
+
+        res.json({ 
+            message: 'Card created!',
+            fullText: clozeCards[clozeCards.length-1].fullText,
+            cloze: clozeCards[clozeCards.length-1].cloze,
+            partialText: clozeCards[clozeCards.length-1].partialText
+        });
+        // res.json({cloze: req.body.cloze});
+        });
+        
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
 function BasicCard(front, back) {
     this.front = front;
     this.back = back;
@@ -16,7 +81,6 @@ function ClozeCard(fullText, cloze) {
 }
 
 
-const firstCard = new ClozeCard("This is the full text", "full");
 
 //put BasicCard
 //put ClozeCard
@@ -28,4 +92,4 @@ const firstCard = new ClozeCard("This is the full text", "full");
 //get ClozeCard
 
 
-console.log(firstCard.partialText);
+// console.log(firstCard.partialText);
